91bae904cd89a92c81958140cf7ebd8298128eb5
elenavonder <evonder13@gmail.com>
2020-12-29 21:03:20 -0500

    added to account

--- Account.cs
+++ Account.cs
@@ -3,6 +3,10 @@ namespace Banking.Net5
 {
     public class Account
     {
+
+        private static int NextId = 1;
+        public int Id { get; set; };
+
         public Account()
         {
         }
