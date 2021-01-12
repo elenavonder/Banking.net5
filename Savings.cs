dcbf04e72ffc44e63bb7e0bf9fc445533c1eda36
elenavonder <evonder13@gmail.com>
2021-01-11 19:57:14 -0500

    added method and instance

--- Savings.cs
+++ Savings.cs
@@ -3,6 +3,14 @@ namespace Banking.Net5
 {
     public class Savings : Account
     {
+
+        public double InterestRate { get; protected set; } = 0.01;
+
+        public double CalculateInterest (int months)
+        {
+            return this.Balance * (this.InterestRate / 12) * months;
+        }
+
         public Savings()
         {
         }
