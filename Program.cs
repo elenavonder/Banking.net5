945456686dca2badd157664c42533926d9117216
elenavonder <evonder13@gmail.com>
2021-01-21 14:02:38 -0500

    new instance

--- Program.cs
+++ Program.cs
@@ -8,7 +8,13 @@ namespace Banking.Net5
         {
             var sav1 = new Savings(0.12, "My Savings");
             sav1.Deposit(1000);
+            sav1.PayInterest(1);
 
+            var sav2 = new Savings(0.12, "My Comp Savings");
+            sav2.Deposit(1000);
+            sav2.PayInterest(1);
+
+            var acct1 = new Account();
 
         }
     }
