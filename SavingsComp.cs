70fd81aa6a14d778b6d0295756748fc424c253c4
elenavonder <evonder13@gmail.com>
2021-01-22 22:16:12 -0500

    added property

--- SavingsComp.cs
+++ SavingsComp.cs
@@ -3,6 +3,8 @@ namespace Banking.Net5
 {
     public class SavingsComp
     {
+        public Account account { get; private set; } = null;
+        public double InterestRate { get; private set; } = 0.01;
 
         public SavingsComp()
         {
