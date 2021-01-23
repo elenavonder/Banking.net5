b814b1e9b39e9812701d81cf2b04eaaca44cd43f
elenavonder <evonder13@gmail.com>
2021-01-22 22:17:50 -0500

    Added constructor

--- SavingsComp.cs
+++ SavingsComp.cs
@@ -6,8 +6,10 @@ namespace Banking.Net5
         public Account account { get; private set; } = null;
         public double InterestRate { get; private set; } = 0.01;
 
+
         public SavingsComp()
         {
+            this.account = new Account();
         }
     }
 }
